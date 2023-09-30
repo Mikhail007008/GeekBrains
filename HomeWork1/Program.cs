92e15ed9449a74707d6f28afbba8d6ff287af5f0
Mikhail007008 <kasseta136@mail.ru>
2023-09-30 19:39:19 +0300

    init repository

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Task №2
+
+// Console.Clear();
+// Console.Write("Введите число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b) Console.Write($"min = {b} max = {a}");
+// else Console.Write($"min = {a} max = {b}");
+
+// ------------------------------------------------
+
+// Task №4
+
+// Console.Clear();
+// Console.WriteLine("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число ");
+// int num3 = Convert.ToInt32(Console.ReadLine()),
+//     max = num1;
+
+// for (int i = 0; i <= 2; i++)
+// {
+//     if (max < num2) max = num2;
+//     else if (max < num3) max = num3;
+// }
+
+// Console.WriteLine(max);
+
+// ------------------------------------------------
+
+// Task №6
+
+// Console.Clear();
+// Console.WriteLine("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 != 0) Console.WriteLine("Нет");
+// else Console.WriteLine("Да");
+
+// ------------------------------------------------
+
+// Task №8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()),
+    i = 1;
+
+while (i <= num)
+{
+    if (i % 2 == 0) Console.Write(i + " ");
+    i++;
+}
---
+++ HomeWork2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Task 10
+
+// Console.Clear();
+// Console.WriteLine("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num > 99 && num < 1000) Console.WriteLine($"Вторая цифра числа = {num / 10 % 10}");
+// else Console.WriteLine("Ошибка");
+
+// Task 13
+
+// Console.Clear();
+// Console.WriteLine("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// while (num > 999) num /= 10;
+// if (num < 100) Console.Write("Третьей цифры нет");
+// else Console.WriteLine(num % 10);
+
+//Task 15
+
+string func(int x)
+{
+    for (int i = 1; i <= x; i++)
+        {
+            if (x >= 1 && x <= 5) return "Нет, это не выходной";
+            if (x == 6 || x == 7) return "Да, это выходной";
+        }
+    return "Нет такого дня недели";
+}
+
+Console.WriteLine("Введите цифру, обозначающую день недели ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(func(num));
