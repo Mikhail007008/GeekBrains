a0c02b7c9041d56a7fea72230c556f64f4caa62b
Mikhail007008 <kasseta136@mail.ru>
2023-10-08 19:17:36 +0300

    did homework 4

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,33 +1,33 @@
 ﻿//Task 25
 
-// Console.Clear();
-// Console.WriteLine("Введите первое число ");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число ");
-// int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите первое число ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int B = Convert.ToInt32(Console.ReadLine());
 
-// bool checkValues(int num1, int num2)
-// {
-// 	if (num1 != 0 && num2 > 0) return true;
-// 	return false;
-// }
+bool checkValues(int num1, int num2)
+{
+	if (num1 != 0 && num2 > 0) return true;
+	return false;
+}
 
-// int compute(int a, int b)
-// {
-// 	int i = 1,
-// 		result = 1;
-// 	while (i <= b)
-// 	{
-// 		result *= a;
-// 		i++;
-// 	}
-// 	return result;
-// }
+int compute(int a, int b)
+{
+	int i = 1,
+		result = 1;
+	while (i <= b)
+	{
+		result *= a;
+		i++;
+	}
+	return result;
+}
 
-// if (checkValues(num1, num2))
-// {
-// 	Console.WriteLine(compute(num1, num2));
-// }
+if (checkValues(A, B))
+{
+	Console.WriteLine(compute(A, B));
+}
 
 //Task 27
 
@@ -50,19 +50,19 @@
 
 //Task29
 
-Console.Clear();
-Console.WriteLine("Введите число, задающую длину массива ");
-int size = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("Введите число, задающую длину массива ");
+// int size = Convert.ToInt32(Console.ReadLine());
 
-void func (int num)
-{
-	int [] array = new int[num];
-	for (int i = 0; i < array.Length; i++)
-	{
-		Console.WriteLine("Введите элемент массива ");
-		array[i] = Convert.ToInt32(Console.ReadLine());
-	}
-	Console.WriteLine($"[{string.Join(", ", array)}]");
-}
+// void func (int num)
+// {
+// 	int [] array = new int[num];
+// 	for (int i = 0; i < array.Length; i++)
+// 	{
+// 		Console.WriteLine("Введите элемент массива ");
+// 		array[i] = Convert.ToInt32(Console.ReadLine());
+// 	}
+// 	Console.WriteLine($"[{string.Join(", ", array)}]");
+// }
 
-func(size);
+// func(size);
---
+++ HomeWork4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Task 41
+
+// Console.Clear();
+// Console.WriteLine("Введите числа через пробел ");
+// double[] array = Console.ReadLine().Split(", ").Select( x => double.Parse(x)).ToArray();
+
+// void func(double[] arr)
+// {
+// 	double result = 0;
+// 	for (double i = 0; i < arr.Length; i++)
+// 		if (arr[i] > 0) result++;
+// 	Console.Write(result);
+// }
+
+// func(array);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Задача 43
+
+Console.Clear();
+Console.WriteLine("Введите координаты двух прямых ");
+double[] array = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
+
+void func(double[] arr)
+{
+	double x = 0,
+	y = 0;
+
+	x = (arr[2] - arr[0]) / (arr[1] - arr[3]);
+	y = arr[1] * ((arr[2] - arr[0]) / (arr[1] - arr[3])) + arr[0];
+
+	Console.WriteLine($"({x}; {y})");
+}
+
+func(array);
